b5401f3025394fb32c9a48f06969404d4419930f
thallis-andre <thallis-andre@hotmail.com>
2020-10-22 21:18:34 -0300

    Inserindo o Mongoose

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,11 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
+mongoose.connect('mongodb+srv://trainee:trainee@cluster0.fdaua.mongodb.net/trainee?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
 
 app.get('/', (request, response) => {
   return response.json({

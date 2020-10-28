8ce130eb7c5dda39edf04d5a87dd30972de7c514
thallis-andre <thallis-andre@hotmail.com>
2020-10-28 11:02:33 -0300

    Adicionando Home nas rotas

--- frontend/src/routes.js
+++ frontend/src/routes.js
@@ -11,6 +11,7 @@ export default function Routes() {
         <Switch>
             <Route path="/" exact component={Home} />
             <Route path="/profile" exact component={Profile} />
+            <Route path="/home" exact component={Home} />
             <Route path="/login" exact component={Estrutura}/>
         </Switch>
     );

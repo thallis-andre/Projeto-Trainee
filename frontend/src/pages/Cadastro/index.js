458908df4d1cfaacf7e2ad9fa4286a9fb01b9813
2timesfer <fercastronandes@gmail.com>
2020-10-29 09:12:16 -0300

    esboço do cadastro

--- frontend/src/pages/Cadastro/index.js
+++ frontend/src/pages/Cadastro/index.js
@@ -3,5 +3,18 @@ import React from 'react';
 import './styles.css';
 
 export default function Cadastro(){
-    
+    return(
+        <div className="cadastro-container">
+            <section className="form">
+                <form>
+                    <h1>Faça seu cadastro</h1>
+                    <input placeholder="Nome"/>
+                    <input placeholder="Seu melhor e-mail"/>
+                    <input placeholder="Senha"/>
+                    <button className="button">Concluir cadastro</button>
+
+                </form>
+            </section>
+        </div>
+    )
 }

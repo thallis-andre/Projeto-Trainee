b4d6576e45111c613aba86a2b685497592962708
2timesfer <fercastronandes@gmail.com>
2020-11-03 14:52:07 -0300

    inputs

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 
 import './styles.css';
 import Header from '../Header'
 import Upload from '../DisplayImage';
+import jwt_decode from 'jwt-decode';
 
 
 function Profile() {
+
+  const [user, setUser] = useState();
+
+  function getDecodedToken(){
+    const tokenLocal = localStorage.getItem('token')
+    if(tokenLocal){
+      setUser(jwt_decode(tokenLocal))
+    } else{
+      setUser(null)
+    }
+  }
+
+  useEffect(() =>{
+    getDecodedToken()
+  }, []);
   return (
       <>    
       <Header />
@@ -13,6 +29,14 @@ function Profile() {
       <div>
          < Upload />
       </div>
+      <input type="text"
+        className= "nome"
+        value={user.name}
+        disabled="true" />
+      <input type="text"
+       className="email"
+       value={user.email}
+       disabled="true"/>    
     </>
   );
 }
